c89623fa24328946daa5b8fbab6e1dd744d45b1c
Keitaro Nishijima <keitaro_nishijima@brown.edu>
2022-05-12 01:16:08 -0400

    good

--- code/ConvBlock.py
+++ code/ConvBlock.py
@@ -5,7 +5,7 @@ class ConvBlock(tf.keras.layers.Layer):
     def __init__(self, num_filters):
         super(ConvBlock, self).__init__()
         self.num_filters = num_filters
-        self.initializer = tf.random_normal_initializer(0., 0.02)
+        self.initializer = tf.random_normal_initializer(0.0, 0.02)
         self.conv2D = tf.keras.layers.Conv2D(filters=self.num_filters,kernel_size=3,strides=1,padding='valid',use_bias=False,kernel_initializer=self.initializer)
         self.normalizer = batch_norm()
         
